d99f0cec40fab75c7c8da639b4a84573f851521b
Heiko Goes <heiko.goes@gaebler-goes.de>
2024-01-10 17:41:05 +0100

    first star

--- day01/lib/day01.ex
+++ day01/lib/day01.ex
@@ -2,10 +2,30 @@ defmodule Day01 do
   def solve1(filename) do
     filename
     |> read_input()
+    |> Enum.map(&calc_calibration_value/1)
+    |> Enum.sum()
   end
+
+  def calc_calibration_value(string) do
+    (find_first_digit(string) <> find_first_digit(String.reverse(string)))
+    |> String.to_integer()
+  end
+
+  def find_first_digit(string) when is_binary(string) do
+    string
+    |> to_charlist()
+    |> Enum.find(nil, fn x -> digit?(x) end)
+    |> to_digit()
+    |> Integer.to_string()
+  end
+
+  defp digit?(char), do: char in ?0..?9
+
+  defp to_digit(char), do: char - ?0
 
   def read_input(filename) do
     filename
     |> File.read!()
+    |> String.split("\n", trim: true)
   end
 end
--- day01/test/day01_test.exs
+++ day01/test/day01_test.exs
@@ -1,7 +1,15 @@
 defmodule Day01Test do
   use ExUnit.Case
 
-  test "" do
-    # assert Day01.hello() == :world
+  test "find_first_digit" do
+    assert Day01.find_first_digit("hallo52as") == "5"
+  end
+
+  test "solve sample1" do
+    assert Day01.solve1("sample1") == 142
+  end
+
+  test "solve star1" do
+    assert Day01.solve1("star1") == 142
   end
 end
