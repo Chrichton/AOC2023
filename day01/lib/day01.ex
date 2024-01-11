a2c6bd319e1953661076a09cd4d69583b9f46aef
Heiko Goes <heiko.goes@gaebler-goes.de>
2024-01-11 11:06:18 +0100

    second star

--- day01/lib/day01.ex
+++ day01/lib/day01.ex
@@ -2,16 +2,16 @@ defmodule Day01 do
   def solve1(filename) do
     filename
     |> read_input()
-    |> Enum.map(&calc_calibration_value/1)
+    |> Enum.map(&calc_calibration_value1/1)
     |> Enum.sum()
   end
 
-  def calc_calibration_value(string) do
-    (find_first_digit(string) <> find_first_digit(String.reverse(string)))
+  def calc_calibration_value1(string) do
+    (find_first_digit1(string) <> find_first_digit1(String.reverse(string)))
     |> String.to_integer()
   end
 
-  def find_first_digit(string) when is_binary(string) do
+  def find_first_digit1(string) when is_binary(string) do
     string
     |> to_charlist()
     |> Enum.find(nil, fn x -> digit?(x) end)
@@ -22,6 +22,68 @@ defmodule Day01 do
   defp digit?(char), do: char in ?0..?9
 
   defp to_digit(char), do: char - ?0
+
+  # Second star ----------------------------------------------------------------------
+
+  @numbers %{
+    "zero" => "0",
+    "one" => "1",
+    "two" => "2",
+    "three" => "3",
+    "four" => "4",
+    "five" => "5",
+    "six" => "6",
+    "seven" => "7",
+    "eight" => "8",
+    "nine" => "9",
+    "0" => "0",
+    "1" => "1",
+    "2" => "2",
+    "3" => "3",
+    "4" => "4",
+    "5" => "5",
+    "6" => "6",
+    "7" => "7",
+    "8" => "8",
+    "9" => "9"
+  }
+
+  @reverse_numbers Map.new(@numbers, fn {key, value} -> {String.reverse(key), value} end)
+
+  def solve2(filename) do
+    filename
+    |> read_input()
+    |> Enum.map(&calc_calibration_value/1)
+    |> Enum.sum()
+  end
+
+  def calc_calibration_value(string) do
+    (find_first_digit(string, false) <> find_first_digit(string, true))
+    |> String.to_integer()
+  end
+
+  def find_first_digit(string, reverse?) do
+    numbers =
+      if reverse?,
+        do: @reverse_numbers,
+        else: @numbers
+
+    string =
+      if reverse?,
+        do: String.reverse(string),
+        else: string
+
+    numbers
+    |> Map.keys()
+    |> Enum.reduce([], fn number_string, acc ->
+      case :binary.match(string, number_string) do
+        :nomatch -> acc
+        {index, _lenght} -> [{index, Map.get(numbers, number_string)} | acc]
+      end
+    end)
+    |> Enum.min_by(fn {index, _number_string} -> index end)
+    |> elem(1)
+  end
 
   def read_input(filename) do
     filename
--- day01/test/day01_test.exs
+++ day01/test/day01_test.exs
@@ -2,7 +2,11 @@ defmodule Day01Test do
   use ExUnit.Case
 
   test "find_first_digit" do
-    assert Day01.find_first_digit("hallo52as") == "5"
+    assert Day01.find_first_digit("hallo52as", false) == "5"
+  end
+
+  test "find_first_digit reverse" do
+    assert Day01.find_first_digit("hallo52as", true) == "2"
   end
 
   test "solve sample1" do
@@ -11,5 +15,23 @@ defmodule Day01Test do
 
   test "solve star1" do
     assert Day01.solve1("star1") == 54159
+  end
+
+  # Second star ----------------------------------------------
+
+  test "solve sample2" do
+    assert Day01.solve2("sample2") == 281
+  end
+
+  test "find_first_digit with text number" do
+    assert Day01.find_first_digit("xtwone3four", false) == "2"
+  end
+
+  test "find_first_digit with text number revers" do
+    assert Day01.find_first_digit("xtwone3four", true) == "4"
+  end
+
+  test "solve star2" do
+    assert Day01.solve2("star1") == 53866
   end
 end
