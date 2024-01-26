8281325bd756e1cc475fbc17df6c87ae14f22ec1
Heiko Goes <heiko.goes@gaebler-goes.de>
2024-01-26 17:01:16 +0100

    second star

--- day04/lib/day04.ex
+++ day04/lib/day04.ex
@@ -2,7 +2,8 @@ defmodule Day04 do
   def solve1(filename) do
     filename
     |> read_input()
-    |> calc_points()
+    |> Enum.map(&calc_points/1)
+    |> Enum.sum()
   end
 
   def read_input(filename) do
@@ -28,18 +29,65 @@ defmodule Day04 do
     |> MapSet.new()
   end
 
-  def calc_points(map_sets) do
-    map_sets
-    |> Enum.map(fn {winning_numbers_map_set, my_numbers_map_set} ->
-      MapSet.intersection(winning_numbers_map_set, my_numbers_map_set)
-      |> Enum.count()
-      |> power2()
-    end)
-    |> Enum.sum()
+  def calc_my_winning_numbers_count({winning_numbers_map_set, my_numbers_map_set}) do
+    MapSet.intersection(winning_numbers_map_set, my_numbers_map_set)
+    |> Enum.count()
+  end
+
+  def calc_points({winning_numbers_map_set, my_numbers_map_set}) do
+    {winning_numbers_map_set, my_numbers_map_set}
+    |> calc_my_winning_numbers_count()
+    |> power2()
   end
 
   def power2(0), do: 0
+
   def power2(n) do
     Integer.pow(2, n - 1)
-  end    
+  end
+
+  # --------------------------------------
+
+  def solve2(filename) do
+    map_sets =
+      filename
+      |> read_input()
+
+    card_counts_map =
+      map_sets
+      |> Enum.count()
+      |> create_card_count_map()
+
+    map_sets
+    |> Enum.zip(1..Enum.count(map_sets))
+    |> Enum.reduce(card_counts_map, fn
+      {{winning_numbers_map_set, my_numbers_map_set}, card_no}, acc ->
+        winners = calc_my_winning_numbers_count({winning_numbers_map_set, my_numbers_map_set})
+
+        if winners > 0 do
+          increment_card_counts(
+            acc,
+            Range.new(card_no + 1, card_no + winners),
+            Map.get(acc, card_no)
+          )
+        else
+          acc
+        end
+    end)
+    |> Map.values()
+    |> Enum.sum()
+  end
+
+  def increment_card_counts(card_counts_map, range, increment) do
+    range
+    |> Enum.reduce(card_counts_map, fn card_no, acc ->
+      Map.update(acc, card_no, increment, fn old_value ->
+        old_value + increment
+      end)
+    end)
+  end
+
+  def create_card_count_map(card_count) do
+    for n <- 1..card_count, into: %{}, do: {n, 1}
+  end
 end
--- day04/test/day04_test.exs
+++ day04/test/day04_test.exs
@@ -6,13 +6,30 @@ defmodule Day04Test do
 
     assert winning_numbers1 == MapSet.new([41, 48, 83, 86, 17])
     assert my_numbers1 == MapSet.new([83, 86, 6, 31, 17, 9, 48, 53])
- end
+  end
 
-  test "sample" do
+  test "sample1" do
     assert Day04.solve1("sample") == 13
   end
 
-  test "star" do
+  test "star1" do
     assert Day04.solve1("star") == 20855
+  end
+
+  test "create_card_count_map" do
+    assert Day04.create_card_count_map(4) == %{1 => 1, 2 => 1, 3 => 1, 4 => 1}
+  end
+
+  test "update_card_counts" do
+    assert Day04.increment_card_counts(%{3 => 1}, 2..4, 3) ==
+             %{2 => 3, 3 => 4, 4 => 3}
+  end
+
+  test "sample2" do
+    assert Day04.solve2("sample") == 30
+  end
+
+  test "star2" do
+    assert Day04.solve2("star") == 5_489_600
   end
 end
