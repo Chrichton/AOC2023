e58ec65af722af020ff2bb6a5fe387d93c894eab
Heiko Goes <heiko.goes@gaebler-goes.de>
2024-02-05 15:56:05 +0100

    first star

---
+++ day08/lib/day08.ex
@@ -0,0 +1,45 @@
+defmodule Day08 do
+  def read_input(input) do
+    input = File.read!(input)
+
+    [direction_string | [_blank_line | from_tos]] = String.split(input, "\n")
+
+    directions =
+      direction_string
+      |> String.codepoints()
+      |> Stream.cycle()
+
+    left_rights =
+      Enum.map(from_tos, fn from_to ->
+        [from | [to]] = String.split(from_to, " = (")
+        [left | [right]] = String.split(to, ", ")
+        right = String.replace(right, ")", "")
+        {from, {left, right}}
+      end)
+      |> Map.new()
+
+    {directions, left_rights}
+  end
+
+  def solve(input) do
+    input
+    |> read_input()
+    |> then(fn {directions, left_rights} ->
+      Enum.reduce_while(directions, {"AAA", 1}, fn direction, {from, steps} ->
+        destination = destination(from, direction, left_rights)
+
+        if destination == "ZZZ",
+          do: {:halt, steps},
+          else: {:cont, {destination, steps + 1}}
+      end)
+    end)
+  end
+
+  def destination(from, left_right, left_rights) do
+    left_right_pair = Map.get(left_rights, from)
+
+    if left_right == "L",
+      do: elem(left_right_pair, 0),
+      else: elem(left_right_pair, 1)
+  end
+end
---
+++ day08/test/day08_test.exs
@@ -0,0 +1,16 @@
+defmodule Day08Test do
+  use ExUnit.Case
+
+  test "read_input" do
+    assert Day08.read_input("sample") |> elem(1) ==
+             %{"AAA" => {"BBB", "BBB"}, "BBB" => {"AAA", "ZZZ"}, "ZZZ" => {"ZZZ", "ZZZ"}}
+  end
+
+  test "sample" do
+    assert Day08.solve("sample") == 6
+  end
+
+  test "star" do
+    assert Day08.solve("star") == 6
+  end
+end
