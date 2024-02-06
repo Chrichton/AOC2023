21863f25059e8d2f552ec22a61653466d0c5ac69
Heiko Goes <heiko.goes@gaebler-goes.de>
2024-02-06 16:01:59 +0100

    refactorings

--- day09/lib/day09.ex
+++ day09/lib/day09.ex
@@ -13,26 +13,15 @@ defmodule Day09 do
   def solve(input) do
     input
     |> read_input()
-    |> Enum.map(fn line -> next_lines(line, [line]) end)
+    |> Enum.map(fn line -> add_next_line([line]) end)
     |> Enum.map(fn lines_list ->
-      Enum.map(lines_list, fn lines ->
-        last = List.last(lines)
-
-        if last == nil do
-          IO.inspect(lines_list)
-          0
-        else
-          last
-        end
-      end)
+      Enum.map(lines_list, fn lines -> List.last(lines) end)
       |> Enum.sum()
     end)
     |> Enum.sum()
   end
 
-  def next_lines(line, lines) do
-    if line == [], do: IO.inspect(lines)
-
+  def add_next_line([line | _rest] = lines) do
     next_line =
       Enum.zip(Enum.take(line, Enum.count(line) - 1), Enum.drop(line, 1))
       |> Enum.reduce([], fn {top, bottom}, acc -> [bottom - top | acc] end)
@@ -42,6 +31,6 @@ defmodule Day09 do
 
     if Enum.all?(next_line, &(&1 == 0)),
       do: Enum.reverse(lines),
-      else: next_lines(next_line, lines)
+      else: add_next_line(lines)
   end
 end
