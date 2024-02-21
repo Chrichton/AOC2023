d9db5756d2bc5777a79eab3cc756769cd912c6d1
Heiko Goes <heiko.goes@gaebler-goes.de>
2024-02-21 16:16:08 +0100

    first star

--- day24/lib/day24.ex
+++ day24/lib/day24.ex
@@ -1,54 +1,93 @@
 defmodule Day24 do
-  def hailstorms(),
-    do: [
-      {{19, 13}, {-2, 1}},
-      {{18, 19}, {-1, -1}},
-      {{20, 25}, {-2, -2}},
-      {{12, 31}, {-1, -2}},
-      {{20, 19}, {1, -5}}
-    ]
+  def read_input(input) do
+    input
+    |> File.read!()
+    |> String.split("\n")
+    |> Enum.map(fn line ->
+      [position, velocity] =
+        String.split(line, " @ ")
 
-  def solve() do
-    # Enum.map(
-    #   [
-    #     {{19, 13}, {-2, 1}},
-    #     {{18, 19}, {-1, -1}}
-    #   ],
-    #   &time_tick/1
-    # )
-
-    time_tick({{20, 19}, {1, -5}})
+      [string_to_coordinates(position) | [string_to_coordinates(velocity)]]
+    end)
   end
 
-  @test_range 7..27
-
-  def time_tick({{x, y}, {v_x, v_y}}) do
-    {x_next, y_next} = {x + v_x, y + v_y}
-    dx = if x_next > x, do: :x_inc, else: :x_dec
-    dy = if y_next > x, do: :y_inc, else: :y_dec
-
-    time_tick({x, y}, {dx, dy}, {v_x, v_y}, MapSet.new())
+  def string_to_coordinates(string) do
+    string
+    |> String.split(", ")
+    |> Enum.map(fn s ->
+      String.trim(s)
+      |> String.to_integer()
+    end)
+    |> List.to_tuple()
   end
 
-  def time_tick({x, _y}, {:x_inc, _dy}, _v, path) when x > 27,
-    do: path
-
-  def time_tick({_x, y}, {_dx, :y_inc}, _v, path) when y > 27,
-    do: path
-
-  def time_tick({x, _y}, {:x_dec, _dy}, _v, path) when x < 7,
-    do: path
-
-  def time_tick({_x, y}, {_dx, :y_dec}, _v, path) when y < 7,
-    do: path
-
-  def time_tick({x, y}, slope, {v_x, v_y}, path) do
-    path =
-      if x in @test_range and y in @test_range,
-        do: MapSet.put(path, {x, y}),
-        else: path
-
-    {x_next, y_next} = {x + v_x, y + v_y}
-    time_tick({x_next, y_next}, slope, {v_x, v_y}, path)
+  def solve(input, {box_min, box_max}) do
+    input
+    |> read_input()
+    |> Combination.combine(2)
+    |> Enum.map(fn pair ->
+      find_intersection_point(pair)
+    end)
+    |> Enum.filter(fn {key, _value} -> key == :fine end)
+    |> Enum.map(fn {_key, value} -> value end)
+    |> Enum.filter(fn {x, y} -> in_box?({x, y}, {box_min, box_max}) end)
+    |> Enum.count()
   end
+
+  def in_box?({x, y}, {min, max}),
+    do: x >= min and x <= max and y >= min and y <= max
+
+  def find_intersection_point([[{x0, y0, _z0}, {u0, v0, _w0}], [{x1, y1, _z1}, {u1, v1, _w1}]]) do
+    if u0 * v1 - u1 * v0 == 0 do
+      {:parallel, nil}
+    else
+      t = ((x1 - x0) * v1 - (y1 - y0) * u1) / (u0 * v1 - v0 * u1)
+      s = ((x1 - x0) * v0 - (y1 - y0) * u0) / (u0 * v1 - v0 * u1)
+
+      cond do
+        t < 0 or s < 0 -> {:past, nil}
+        true -> {:fine, {x0 + t * u0, y0 + t * v0}}
+      end
+    end
+  end
+
+  # def find_intersection_point([[{u0, v0, _w0}, {u1, v1, z1}], [{x0, y0, _z0}, {x1, y1, z1}]]) do
+  #   if u0 * v1 - u1 * v0 == 0 do
+  #     {:parallel, nil}
+  #   else
+  #     t = ((x1 - x0) * v1 - (y1 - y0) * u1) / (u0 * v1 - v0 * u1)
+  #     s = ((x1 - x0) * v0 - (y1 - y0) * u0) / (u0 * v1 - v0 * u1)
+
+  #     cond do
+  #       t < 0 or s < 0 -> {:past, nil}
+  #       true -> {:fine, {x0 + t * u0, y0 + t * v0}}
+  #     end
+  #   end
+  # end
+
+  # def find_intersection_point(
+  #       {v1x, v1y} = _v1,
+  #       {v2x, v2y} = _v2,
+  #       {p1x, p1y} = _p1,
+  #       {p2x, p2y} = _p2
+  #     ) do
+  #   # a = Nx.tensor([[v1x, -v2x], [v1y, -v2y]]) |> IO.inspect(label: "a")
+  #   # b = Nx.tensor([[p2x - p1x], [p2y - p1y]]) |> IO.inspect(label: "b")
+  #   # Nx.LinAlg.solve(a, b)
+
+  #   #   a = det(v1, v2)
+  #   #   b = det({x2 - x1, v1}, {y2 - y1, v1})
+  #   #   if a != 0 do
+  #   #     x = b / a
+  #   #     y = det({x2 - x1, v1}, {x2 - x1, v2}) / a
+  #   #     {x, y}
+  #   #   else
+  #   #     {:error, "Lines are parallel"}
+  #   #   end
+  #   # end
+
+  #   _x = ((p2x - p1x) * -v2y + -v2x * (p2y - p1y)) / (v1x * -v2y + v2x * v1y)
+  # end
+
+  # defp det({a, b}, {c, d}), do: a * d - b * c
 end
--- day24/test/day24_test.exs
+++ day24/test/day24_test.exs
@@ -1,13 +1,101 @@
 defmodule Day24Test do
   use ExUnit.Case
 
-  test "sample" do
-    assert Day24.solve() == nil
-
-    [m1, m2] = Day24.solve()
-    MapSet.intersection(m1, m2)
-
-    assert [m1, m2] == nil
-    assert MapSet.intersection(m1, m2) == nil
+  test "read_input" do
+    assert Day24.read_input("sample") == [
+             [{19, 13, 30}, {-2, 1, -2}],
+             [{18, 19, 22}, {-1, -1, -2}],
+             [{20, 25, 34}, {-2, -2, -4}],
+             [{12, 31, 28}, {-1, -2, -1}],
+             [{20, 19, 15}, {1, -5, -3}]
+           ]
   end
+
+  test "sample" do
+    assert Day24.solve("sample", {7, 27}) == 2
+  end
+
+  test "star" do
+    assert Day24.solve("star", {200_000_000_000_000, 400_000_000_000_000}) == 27732
+  end
+
+  test "sample usage" do
+    # Example usage
+    # Direction vector of line 1
+    v1 = {-2, 1, 0}
+    # Direction vector of line 2
+    v2 = {-1, -1, 0}
+    # Point on line 1
+    p1 = {19, 13, 0}
+    # Point on line 2
+    p2 = {18, 19, 0}
+
+    intersection_point = Day24.find_intersection_point([[p1, v1], [p2, v2]])
+    # Output: {14.333, y=15.333}
+    IO.inspect(intersection_point)
+  end
+
+  # test "sample" do
+  #   # Example usage
+  #   # Direction vector of line 1
+  #   v1 = {-2, 1}
+  #   # Direction vector of line 2
+  #   v2 = {-1, -1}
+  #   # Point on line 1
+  #   p1 = {19, 13}
+  #   # Point on line 2
+  #   p2 = {18, 19}
+
+  #   intersection_point = Day24.find_intersection_point(v1, v2, p1, p2)
+  #   # Output: {14.333, y=15.333}
+  #   IO.inspect(intersection_point)
+  # end
+
+  # test "sample" do
+  #   # Example usage
+  #   # Direction vector of line 1
+  #   v1 = {-2, 1}
+  #   # Direction vector of line 2
+  #   v2 = {-2, -2}
+  #   # Point on line 1
+  #   p1 = {19, 13}
+  #   # Point on line 2
+  #   p2 = {12, 31}
+
+  #   intersection_point = Day24.find_intersection_point(v1, v2, p1, p2)
+  #   # Output: {14.333, y=15.333}
+  #   IO.inspect(intersection_point)
+  # end
+
+  # test "sample" do
+  #   # Example usage
+  #   # Direction vector of line 1
+  #   v1 = {1, 1}
+  #   # Direction vector of line 2
+  #   v2 = {1, 0}
+  #   # Point on line 1
+  #   p1 = {1, 3}
+  #   # Point on line 2
+  #   p2 = {5, 7}
+
+  #   intersection_point = Day24.find_intersection_point(v1, v2, p1, p2)
+  #   # Output: {14.333, y=15.333}
+  #   IO.inspect(intersection_point)
+  # end
+
+  # test "sample" do
+  #   # Example usage
+  #   # Direction vector of line 1
+  #   v1 = {3, 5}
+  #   # Direction vector of line 2
+  #   v2 = {-7, 0}
+  #   # Point on line 1
+  #   p1 = {1, 15}
+  #   # Point on line 2
+  #   p2 = {14, 26}
+
+  #   intersection_point = Day24.find_intersection_point(v1, v2, p1, p2)
+  #   # Output: {14.2.200000047683716, y=0.9142857789993286.333}
+  #   IO.inspect(intersection_point)
+  # end
 end
