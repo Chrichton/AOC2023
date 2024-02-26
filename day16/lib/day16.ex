398c85702c3b7487a06c1b97de44a2ccc9589d64
Heiko Goes <heiko.goes@gaebler-goes.de>
2024-02-26 14:18:08 +0100

    sample passes

--- day16/lib/day16.ex
+++ day16/lib/day16.ex
@@ -1,12 +1,50 @@
 defmodule Day16 do
   def read_input(input) do
-    input
-    |> File.read!()
-    |> String.split("\n")
-    |> find_mirrors()
+    lines =
+      input
+      |> File.read!()
+      |> String.split("\n")
+
+    {find_mirrors(lines), max_x_y(lines)}
   end
 
-  def next_rays({{_x, _y} = position, direction} = ray, mirrors) do
+  def solve(input) do
+    input
+    |> read_input()
+    |> next_step(MapSet.new([{{0, 0}, :east}]), MapSet.new([{{0, 0}, :east}]), 1)
+  end
+
+  def next_step({_mirrors, {_max_x, _max_y}}, _rays, visited, 0) do
+    visited
+    |> Enum.map(fn {position, _direction} -> position end)
+    |> MapSet.new()
+    |> Enum.count()
+  end
+
+  def next_step({mirrors, {max_x, max_y}}, rays, visited, _rays_count) do
+    IO.inspect(rays, label: "rays")
+    IO.inspect(visited, label: "visited")
+
+    rays
+    |> Enum.reduce(
+      {MapSet.new(), visited},
+      fn ray, {acc_rays, acc_visited} ->
+        next_rays =
+          next_rays(ray, mirrors, max_x, max_y)
+          |> MapSet.new()
+          |> MapSet.difference(visited)
+
+        next_visited = MapSet.new(next_rays)
+
+        {MapSet.union(acc_rays, next_rays), MapSet.union(acc_visited, next_visited)}
+      end
+    )
+    |> then(fn {new_rays, new_visited} ->
+      next_step({mirrors, {max_x, max_y}}, new_rays, new_visited, Enum.count(new_rays))
+    end)
+  end
+
+  def next_rays({{_x, _y} = position, direction} = ray, mirrors, max_x, max_y) do
     case Map.fetch(mirrors, position) do
       :error ->
         [next_ray(ray)]
@@ -15,7 +53,7 @@ defmodule Day16 do
         if direction in [:west, :east] do
           [next_ray(ray)]
         else
-          [next_ray({position, :north}), next_ray({position, :south})]
+          [next_ray({position, :west}), next_ray({position, :east})]
         end
 
       {:ok, "|"} ->
@@ -34,10 +72,10 @@ defmodule Day16 do
             [next_ray({position, :south})]
 
           :south ->
-            [next_ray({position, :east})]
+            [next_ray({position, :west})]
 
           :north ->
-            [next_ray({position, :west})]
+            [next_ray({position, :east})]
         end
 
       {:ok, "\\"} ->
@@ -49,16 +87,18 @@ defmodule Day16 do
             [next_ray({position, :north})]
 
           :south ->
-            [next_ray({position, :west})]
+            [next_ray({position, :east})]
 
           :north ->
-            [next_ray({position, :east})]
+            [next_ray({position, :west})]
         end
     end
-    |> Enum.reject(fn {{x1, y1}, _direction} -> x1 < 0 or y1 < 0 end)
+    |> Enum.filter(fn {{x1, y1}, _direction} ->
+      x1 in 0..max_x and y1 in 0..max_y
+    end)
   end
 
-  def next_ray({{x, y}, direction} = _ray) do
+  defp next_ray({{x, y}, direction} = _ray) do
     cond do
       direction == :north -> {{x, y - 1}, :north}
       direction == :south -> {{x, y + 1}, :south}
@@ -67,11 +107,23 @@ defmodule Day16 do
     end
   end
 
-  def find_mirrors(lines) do
+  defp find_mirrors(lines) do
     for {line, y_index} <- Enum.with_index(lines),
         {char, x_index} <- Enum.with_index(String.codepoints(line)),
         char != ".",
         into: %{},
         do: {{x_index, y_index}, char}
+  end
+
+  defp max_x_y(lines) do
+    max_x =
+      lines
+      |> List.first()
+      |> String.length()
+      |> Kernel.-(1)
+
+    max_y = Enum.count(lines) - 1
+
+    {max_x, max_y}
   end
 end
--- day16/test/day16_test.exs
+++ day16/test/day16_test.exs
@@ -3,54 +3,62 @@ defmodule Day16Test do
   doctest Day16
 
   test "read_input" do
-    assert Day16.read_input("sample") == %{
-             {0, 1} => "|",
-             {1, 0} => "|",
-             {1, 7} => "-",
-             {1, 8} => "|",
-             {2, 1} => "-",
-             {2, 9} => "/",
-             {3, 7} => "-",
-             {3, 9} => "/",
-             {4, 1} => "\\",
-             {4, 6} => "/",
-             {4, 7} => "/",
-             {5, 0} => "\\",
-             {5, 2} => "|",
-             {5, 9} => "|",
-             {6, 2} => "-",
-             {6, 6} => "\\",
-             {6, 8} => "-",
-             {7, 6} => "\\",
-             {7, 7} => "|",
-             {7, 8} => "|",
-             {8, 3} => "|",
-             {9, 5} => "\\",
-             {9, 8} => "\\"
-           }
+    assert Day16.read_input("sample") ==
+             {
+               %{
+                 {0, 1} => "|",
+                 {1, 0} => "|",
+                 {1, 7} => "-",
+                 {1, 8} => "|",
+                 {2, 1} => "-",
+                 {2, 9} => "/",
+                 {3, 7} => "-",
+                 {3, 9} => "/",
+                 {4, 1} => "\\",
+                 {4, 6} => "/",
+                 {4, 7} => "/",
+                 {5, 0} => "\\",
+                 {5, 2} => "|",
+                 {5, 9} => "|",
+                 {6, 2} => "-",
+                 {6, 6} => "\\",
+                 {6, 8} => "-",
+                 {7, 6} => "\\",
+                 {7, 7} => "|",
+                 {7, 8} => "|",
+                 {8, 3} => "|",
+                 {9, 5} => "\\",
+                 {9, 8} => "\\"
+               },
+               {9, 9}
+             }
   end
 
   test "next_rays ." do
-    assert Day16.next_rays({{0, 0}, :east}, Map.new()) == [{{1, 0}, :east}]
+    assert Day16.next_rays({{0, 0}, :east}, Map.new(), 9, 9) == [{{1, 0}, :east}]
   end
 
   test "next_rays |" do
-    assert Day16.next_rays({{1, 0}, :east}, Map.new([{{1, 0}, "|"}])) ==
+    assert Day16.next_rays({{1, 0}, :east}, Map.new([{{1, 0}, "|"}]), 9, 9) ==
              [{{1, 1}, :south}]
   end
 
   test "next_rays -" do
-    assert Day16.next_rays({{1, 0}, :east}, Map.new([{{1, 0}, "-"}])) ==
+    assert Day16.next_rays({{1, 0}, :east}, Map.new([{{1, 0}, "-"}]), 9, 9) ==
              [{{2, 0}, :east}]
   end
 
   test "next_rays \\" do
-    assert Day16.next_rays({{1, 0}, :east}, Map.new([{{1, 0}, "\\"}])) ==
+    assert Day16.next_rays({{1, 0}, :east}, Map.new([{{1, 0}, "\\"}]), 9, 9) ==
              [{{1, 1}, :south}]
   end
 
   test "next_rays /" do
-    assert Day16.next_rays({{1, 0}, :west}, Map.new([{{1, 0}, "/"}])) ==
+    assert Day16.next_rays({{1, 0}, :west}, Map.new([{{1, 0}, "/"}]), 9, 9) ==
              [{{1, 1}, :south}]
+  end
+
+  test "sample" do
+    assert Day16.solve("sample") == 46
   end
 end
