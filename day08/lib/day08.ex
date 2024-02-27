073535e69f62e67d554800ac0a613cf58d3680ba
Heiko Goes <heiko.goes@gaebler-goes.de>
2024-02-27 17:06:52 +0100

    second star

--- day08/lib/day08.ex
+++ day08/lib/day08.ex
@@ -25,13 +25,17 @@ defmodule Day08 do
     input
     |> read_input()
     |> then(fn {directions, left_rights} ->
-      Enum.reduce_while(directions, {"AAA", 1}, fn direction, {from, steps} ->
-        destination = destination(from, direction, left_rights)
+      traverse(directions, left_rights, "AAA", &(&1 == "ZZZ"))
+    end)
+  end
 
-        if destination == "ZZZ",
-          do: {:halt, steps},
-          else: {:cont, {destination, steps + 1}}
-      end)
+  def traverse(directions, left_rights, from, to_func) do
+    Enum.reduce_while(directions, {from, 1}, fn direction, {from, steps} ->
+      destination = destination(from, direction, left_rights)
+
+      if to_func.(destination),
+        do: {:halt, steps},
+        else: {:cont, {destination, steps + 1}}
     end)
   end
 
@@ -45,77 +49,97 @@ defmodule Day08 do
 
   # ----------------------------------------------------------------
 
-  def read_input2(input) do
-    input = File.read!(input)
-
-    [_direction_string | [_blank_line | from_tos]] = String.split(input, "\n")
-
-    left_rights =
-      Enum.map(from_tos, fn from_to ->
-        [from, to] = String.split(from_to, " = (")
-        [left, right] = String.split(to, ", ")
-        right = String.replace(right, ")", "")
-        {from, {left, right}}
-      end)
-
-    left_rights
-  end
-
-  def convert(left_rights) do
-    left_rights
-    |> Enum.reduce({[], %{}, %{}, %{}}, fn {from, {left, right}},
-                                           {result, from_map, left_map, right_map} ->
-      {from, from_map} = update(from, from_map)
-      {left, left_map} = update(left, left_map)
-      {right, right_map} = update(right, right_map)
-      {[{from, {left, right}} | result], from_map, left_map, right_map}
-    end)
-    |> then(fn {result, _from_map, _left_map, _right_map} -> result end)
-    |> Map.new()
-  end
-
   def solve2(input) do
     input
     |> read_input()
     |> then(fn {directions, left_rights} ->
-      starting_nodes =
-        left_rights
-        |> Map.keys()
-        |> Enum.filter(&String.ends_with?(&1, "A"))
-
-      directions
-      |> Enum.reduce_while(
-        {starting_nodes, 1},
-        fn direction, {starting_nodes, steps} ->
-          dest_nodes = destination2(starting_nodes, direction, left_rights)
-
-          if Enum.all?(dest_nodes, &String.ends_with?(&1, "Z")),
-            do: {:halt, steps},
-            else: {:cont, {dest_nodes, steps + 1}}
-        end
-      )
+      Enum.filter(left_rights, fn {left, _right} -> String.ends_with?(left, "A") end)
+      |> Enum.map(fn {left, _right} ->
+        traverse(directions, left_rights, left, &String.ends_with?(&1, "Z"))
+      end)
+      |> lcm()
     end)
   end
 
-  def destination2(starting_nodes, direction, left_rights) do
-    Enum.map(starting_nodes, fn node ->
-      {left, right} = Map.get(left_rights, node)
+  def lcm([number]), do: number
 
-      if direction == "L",
-        do: left,
-        else: right
-    end)
+  def lcm([number | numbers]) do
+    number
+    |> ElixirMath.lcm(lcm(numbers))
   end
 
-  def update(string, map) do
-    last_char = String.last(string)
-    map = Map.update(map, last_char, 1, fn count -> count + 1 end)
+  ########## Too Slow for second star
+  # def read_input2(input) do
+  #   input = File.read!(input)
 
-    count_char =
-      map
-      |> Map.get(last_char)
-      |> Integer.to_string()
+  #   [_direction_string | [_blank_line | from_tos]] = String.split(input, "\n")
 
-    {count_char <> count_char <> last_char, map}
-  end
+  #   left_rights =
+  #     Enum.map(from_tos, fn from_to ->
+  #       [from, to] = String.split(from_to, " = (")
+  #       [left, right] = String.split(to, ", ")
+  #       right = String.replace(right, ")", "")
+  #       {from, {left, right}}
+  #     end)
+
+  #   left_rights
+  # end
+
+  # def convert(left_rights) do
+  #   left_rights
+  #   |> Enum.reduce({[], %{}, %{}, %{}}, fn {from, {left, right}},
+  #                                          {result, from_map, left_map, right_map} ->
+  #     {from, from_map} = update(from, from_map)
+  #     {left, left_map} = update(left, left_map)
+  #     {right, right_map} = update(right, right_map)
+  #     {[{from, {left, right}} | result], from_map, left_map, right_map}
+  #   end)
+  #   |> then(fn {result, _from_map, _left_map, _right_map} -> result end)
+  #   |> Map.new()
+  # end
+
+  # def solve2(input) do
+  #   input
+  #   |> read_input()
+  #   |> then(fn {directions, left_rights} ->
+  #     starting_nodes =
+  #       left_rights
+  #       |> Map.keys()
+  #       |> Enum.filter(&String.ends_with?(&1, "A"))
+
+  #     directions
+  #     |> Enum.reduce_while(
+  #       {starting_nodes, 1},
+  #       fn direction, {starting_nodes, steps} ->
+  #         dest_nodes = destination2(starting_nodes, direction, left_rights)
+
+  #         if Enum.all?(dest_nodes, &String.ends_with?(&1, "Z")),
+  #           do: {:halt, steps},
+  #           else: {:cont, {dest_nodes, steps + 1}}
+  #       end
+  #     )
+  #   end)
+  # end
+
+  # def destination2(starting_nodes, direction, left_rights) do
+  #   Enum.map(starting_nodes, fn node ->
+  #     {left, right} = Map.get(left_rights, node)
+
+  #     if direction == "L",
+  #       do: left,
+  #       else: right
+  #   end)
+  # end
+
+  # def update(string, map) do
+  #   last_char = String.last(string)
+  #   map = Map.update(map, last_char, 1, fn count -> count + 1 end)
+
+  #   count_char =
+  #     map
+  #     |> Map.get(last_char)
+  #     |> Integer.to_string()
+
+  #   {count_char <> count_char <> last_char, map}
+  # end
 end
