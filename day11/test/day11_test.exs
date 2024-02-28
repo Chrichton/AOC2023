e0fd63a74fd9f02728fe9effdd2691f092ccecd7
Heiko Goes <heiko.goes@gaebler-goes.de>
2024-02-28 06:54:47 +0100

    second star

--- day11/test/day11_test.exs
+++ day11/test/day11_test.exs
@@ -1,11 +1,23 @@
 defmodule Day11Test do
   use ExUnit.Case
 
-  test "sample" do
-    assert Day11.solve("sample") == 374
+  test "sample twice as large" do
+    assert Day11.solve("sample", 2) == 374
   end
 
-  test "star" do
-    assert Day11.solve("star") == 9_799_681
+  test "star twice as large" do
+    assert Day11.solve("star", 2) == 9_799_681
+  end
+
+  test "sample 10 times larger" do
+    assert Day11.solve("sample", 10) == 1030
+  end
+
+  test "sample 100 times larger" do
+    assert Day11.solve("sample", 100) == 8410
+  end
+
+  test "star2 1000000 times larger" do
+    assert Day11.solve("star", 1_000_000) == 513_171_773_355
   end
 end
