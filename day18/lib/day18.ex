ad327676ea6719981fb788e6b7356b4ae993ceed
Heiko Goes <heiko.goes@gaebler-goes.de>
2024-03-10 10:10:05 +0100

    size of sample trench correct

--- day18/lib/day18.ex
+++ day18/lib/day18.ex
@@ -16,32 +16,53 @@ defmodule Day18 do
     |> Enum.map(&parse_line/1)
   end
 
-  def solve(input) do
-    input
-    |> read_input()
-    |> Enum.reduce(
-      {{0, 0}, MapSet.new([{0, 0}])},
-      fn %Command{direction: direction, distance: distance}, {last_position, positions} ->
-        next_position = next_position(last_position, direction, distance)
-        {next_position, MapSet.put(positions, next_position)}
-      end
-    )
-  end
-
-  def next_position({x_last, y_last}, direction, distance) do
-    case direction do
-      "R" -> {x_last + distance, y_last}
-      "L" -> {x_last - distance, y_last}
-      "U" -> {x_last, y_last - distance}
-      "D" -> {x_last, y_last + distance}
-    end
-  end
-
   def parse_line(line) do
     [direction, distance, color_code] = String.split(line, " ")
 
     distance = String.to_integer(distance)
     color_code = String.slice(color_code, 1, String.length(line) - 6)
     Command.new(direction, distance, color_code)
+  end
+
+  def solve(input) do
+    input
+    |> read_input()
+    |> Enum.reduce(
+      {{0, 0}, MapSet.new([{0, 0}])},
+      fn %Command{direction: direction, distance: distance}, {last_position, positions} ->
+        {last_position, next_positions} = next_positions(last_position, direction, distance)
+        {last_position, MapSet.union(positions, MapSet.new(next_positions))}
+      end
+    )
+    |> elem(1)
+  end
+
+  def next_positions(last_position, direction, distance) do
+    case direction do
+      "R" -> next_x_positions(last_position, distance, fn n, m -> n + m end)
+      "L" -> next_x_positions(last_position, distance, fn n, m -> n - m end)
+      "U" -> next_y_positions(last_position, distance, fn n, m -> n - m end)
+      "D" -> next_y_positions(last_position, distance, fn n, m -> n + m end)
+    end
+  end
+
+  defp next_x_positions({x_last, y_last}, distance, sum_func) do
+    next_positions =
+      Enum.reduce(1..distance, [], fn summand, acc ->
+        [{sum_func.(x_last, summand), y_last} | acc]
+      end)
+
+    last_position = hd(next_positions)
+    {last_position, next_positions}
+  end
+
+  defp next_y_positions({x_last, y_last}, distance, sum_func) do
+    next_positions =
+      Enum.reduce(1..distance, [], fn summand, acc ->
+        [{x_last, sum_func.(y_last, summand)} | acc]
+      end)
+
+    last_position = hd(next_positions)
+    {last_position, next_positions}
   end
 end
--- day18/test/day18_test.exs
+++ day18/test/day18_test.exs
@@ -3,24 +3,68 @@ defmodule Day18Test do
 
   test "read_input" do
     assert Day18.read_input("sample") == [
-             %Day18.Command{color_code: "#70c710", direction: "R", distance: "6"},
-             %Day18.Command{color_code: "#0dc571", direction: "D", distance: "5"},
-             %Day18.Command{color_code: "#5713f0", direction: "L", distance: "2"},
-             %Day18.Command{color_code: "#d2c081", direction: "D", distance: "2"},
-             %Day18.Command{color_code: "#59c680", direction: "R", distance: "2"},
-             %Day18.Command{color_code: "#411b91", direction: "D", distance: "2"},
-             %Day18.Command{color_code: "#8ceee2", direction: "L", distance: "5"},
-             %Day18.Command{color_code: "#caa173", direction: "U", distance: "2"},
-             %Day18.Command{color_code: "#1b58a2", direction: "L", distance: "1"},
-             %Day18.Command{color_code: "#caa171", direction: "U", distance: "2"},
-             %Day18.Command{color_code: "#7807d2", direction: "R", distance: "2"},
-             %Day18.Command{color_code: "#a77fa3", direction: "U", distance: "3"},
-             %Day18.Command{color_code: "#015232", direction: "L", distance: "2"},
-             %Day18.Command{color_code: "#7a21e3", direction: "U", distance: "2"}
+             %Day18.Command{color_code: "#70c710", direction: "R", distance: 6},
+             %Day18.Command{color_code: "#0dc571", direction: "D", distance: 5},
+             %Day18.Command{color_code: "#5713f0", direction: "L", distance: 2},
+             %Day18.Command{color_code: "#d2c081", direction: "D", distance: 2},
+             %Day18.Command{color_code: "#59c680", direction: "R", distance: 2},
+             %Day18.Command{color_code: "#411b91", direction: "D", distance: 2},
+             %Day18.Command{color_code: "#8ceee2", direction: "L", distance: 5},
+             %Day18.Command{color_code: "#caa173", direction: "U", distance: 2},
+             %Day18.Command{color_code: "#1b58a2", direction: "L", distance: 1},
+             %Day18.Command{color_code: "#caa171", direction: "U", distance: 2},
+             %Day18.Command{color_code: "#7807d2", direction: "R", distance: 2},
+             %Day18.Command{color_code: "#a77fa3", direction: "U", distance: 3},
+             %Day18.Command{color_code: "#015232", direction: "L", distance: 2},
+             %Day18.Command{color_code: "#7a21e3", direction: "U", distance: 2}
            ]
   end
 
   test "sample" do
-    assert Day18.solve("sample") == nil
+    result = Day18.solve("sample")
+
+    assert MapSet.size(result) == 38
+
+    assert result ==
+             MapSet.new([
+               {4, 5},
+               {5, 9},
+               {1, 2},
+               {2, 4},
+               {6, 5},
+               {5, 0},
+               {0, 5},
+               {0, 1},
+               {4, 0},
+               {6, 1},
+               {2, 0},
+               {4, 6},
+               {6, 2},
+               {0, 7},
+               {0, 0},
+               {6, 8},
+               {1, 7},
+               {2, 3},
+               {1, 8},
+               {4, 7},
+               {3, 0},
+               {4, 9},
+               {6, 0},
+               {5, 5},
+               {1, 9},
+               {6, 4},
+               {1, 0},
+               {1, 5},
+               {2, 5},
+               {2, 2},
+               {5, 7},
+               {6, 9},
+               {0, 2},
+               {0, 6},
+               {6, 7},
+               {6, 3},
+               {3, 9},
+               {2, 9}
+             ])
   end
 end
