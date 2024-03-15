c7ce006663b865c62222f4279b29f87e98c98b2a
Heiko Goes <heiko.goes@gaebler-goes.de>
2024-03-15 17:30:12 +0100

    part1 passes

--- day18_flood/lib/day18_flood.ex
+++ day18_flood/lib/day18_flood.ex
@@ -24,6 +24,17 @@ defmodule Day18Flood do
     Command.new(direction, distance, color_code)
   end
 
+  def shoelace([start_vertex | _rest] = vertices) do
+    (vertices ++ [start_vertex])
+    |> Stream.chunk_every(2, 1, :discard)
+    |> Stream.map(fn [{x_i, y_i}, {x_i_plus_1, y_i_plus_1}] ->
+      x_i * y_i_plus_1 - x_i_plus_1 * y_i
+    end)
+    |> Enum.sum()
+    |> abs()
+    |> div(2)
+  end
+
   def solve(input) do
     trench_map =
       input
@@ -36,21 +47,56 @@ defmodule Day18Flood do
         end
       )
       |> elem(1)
-      |> IO.inspect(limit: :infinity)
 
-    # {{x_min, y_min}, {x_max, y_max}} =
-    #   Enum.min_max(MapSet.to_list(trench_map))
+    vertices =
+      input
+      |> read_input()
+      |> Enum.reduce(
+        {{0, 0}, [{0, 0}]},
+        fn %Command{direction: direction, distance: distance}, {last_position, positions} ->
+          next_position = next_position(last_position, direction, distance)
+          {next_position, [next_position | positions]}
+        end
+      )
+      |> elem(1)
 
-    {{y0, x0}, {y1, x1}} = bounds(trench_map)
+    directions2 = %{
+      "L" => {0, -1},
+      "R" => {0, 1},
+      "U" => {-1, 0},
+      "D" => {1, 0}
+    }
 
-    start =
-      {y0 - 1, x0 - 1}
-      |> IO.inspect()
+    vertices2 =
+      for %Command{direction: direction, distance: distance} <- read_input(input),
+          reduce: [{0, 0}] do
+        [{x, y} | _] = acc ->
+          {dx, dy} = directions2[direction]
+          next_pos = {x + dx * distance, y + dy * distance}
+          [next_pos | acc]
+      end
 
-    # bounds(trench_map)
-    # |> IO.inspect()
+    area =
+      vertices2
+      |> Stream.chunk_every(2, 1, :discard)
+      |> Stream.map(fn [{i1, j1}, {i2, j2}] ->
+        (i1 - i2) * (j1 + j2)
+      end)
+      |> Enum.sum()
+      |> div(2)
+      |> abs()
 
-    # next_step([start], MapSet.new([]), trench_map)
+    perimeter =
+      input
+      |> read_input()
+      |> Enum.map(fn %Command{distance: distance} -> distance end)
+      |> Enum.sum()
+
+    inner_area = shoelace(vertices2)
+
+    perimeter + inner_area
+
+    area + div(perimeter, 2) + 1
   end
 
   def bounds(map) do
@@ -59,23 +105,23 @@ defmodule Day18Flood do
     {{y0, x0}, {y1, x1}}
   end
 
-  def next_step([], visited, trench_map),
-    do: Enum.count(trench_map) + Enum.count(visited)
+  def next_step([], visited, _trench_map, _bounds),
+    do: visited
 
-  def next_step(coords, visited, trench_map) do
+  def next_step(coords, visited, trench_map, bounds) do
     coords
     |> Enum.reduce({[], visited}, fn coord, {acc_coords, acc_visited} ->
-      neighbors(coord, visited, trench_map)
+      neighbors(coord, visited, trench_map, bounds)
       |> Enum.reduce({acc_coords, acc_visited}, fn coord, {acc_coords2, acc_visited2} ->
         {[coord | acc_coords2], MapSet.put(acc_visited2, coord)}
       end)
     end)
     |> then(fn {new_coords, new_visted} ->
-      next_step(new_coords, new_visted, trench_map)
+      next_step(new_coords, new_visted, trench_map, bounds)
     end)
   end
 
-  def neighbors({x, y}, visited, trench_map) do
+  def neighbors({x, y}, visited, trench_map, {{y0, x0}, {y1, x1}}) do
     [
       {x - 1, y - 1},
       {x, y - 1},
@@ -87,13 +133,23 @@ defmodule Day18Flood do
       {x + 1, y + 1}
     ]
     |> Enum.reject(fn neighbor ->
-      MapSet.member?(visited, neighbor) or
+      y < y0 or y > y1 or x < x0 or x > x1 or
+        MapSet.member?(visited, neighbor) or
         MapSet.member?(trench_map, neighbor)
     end)
 
     # |> MapSet.new()
     # |> MapSet.difference(visited)
     # |> MapSet.difference(trench_map)
+  end
+
+  def next_position({x, y} = _last_position, direction, distance) do
+    case direction do
+      "R" -> {x + distance, y}
+      "L" -> {x - distance, y}
+      "U" -> {x, y - distance}
+      "D" -> {x, y + distance}
+    end
   end
 
   def next_positions(last_position, direction, distance) do
@@ -105,7 +161,7 @@ defmodule Day18Flood do
     end
   end
 
-  defp next_x_positions({x_last, y_last}, distance, sum_func) do
+  def next_x_positions({x_last, y_last}, distance, sum_func) do
     next_positions =
       Enum.reduce(1..distance, [], fn summand, acc ->
         [{sum_func.(x_last, summand), y_last} | acc]
@@ -115,7 +171,7 @@ defmodule Day18Flood do
     {last_position, next_positions}
   end
 
-  defp next_y_positions({x_last, y_last}, distance, sum_func) do
+  def next_y_positions({x_last, y_last}, distance, sum_func) do
     next_positions =
       Enum.reduce(1..distance, [], fn summand, acc ->
         [{x_last, sum_func.(y_last, summand)} | acc]
--- day18_flood/test/day18_flood_test.exs
+++ day18_flood/test/day18_flood_test.exs
@@ -1,12 +1,16 @@
 defmodule Day18FloodTest do
   use ExUnit.Case
 
+  test "shoelace" do
+    assert Day18Flood.shoelace([{2, 7}, {10, 1}, {8, 6}, {11, 7}, {7, 10}]) == 32
+  end
+
   test "sample" do
     assert Day18Flood.solve("sample") == 62
   end
 
   @tag timeout: :infinity
   test "star" do
-    assert Day18Flood.solve("star") == 62
+    assert Day18Flood.solve("star") == 50746
   end
 end
